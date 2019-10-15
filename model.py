656c265bd9a96e8041b8ab1455e1dc36781480de
Ryan Arya Pratama <ryan.arya.pratama@gmail.com>
2019-10-15 14:18:19 +0000

    debug: fix output part of datagen

--- model.py
+++ model.py
@@ -12,12 +12,21 @@ import os, csv, math
 import matplotlib.image as mpim
 import numpy as np
 
+def net2():
+    model = Sequential()
+    
+#     model.add(Lambda(lambda x: (x/255)-0.5, input_shape=(160, 320, 3)))
+    model.add(Flatten(input_shape=(160, 320, 3)))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    return model
+
 def network():
     model = Sequential()
     
     # input processing
     # crop (50 px from top, 20 px from bottom)
-    model.add(Cropping2D((50, 20), (0, 0), input_shape=(160, 320, 3)))
+    model.add(Cropping2D(((50, 20), (0, 0)), input_shape=(160, 320, 3)))
     
     # normalise
     model.add(Lambda(lambda x: (x/255)-0.5))
@@ -53,7 +62,7 @@ def network():
     
     # dense3
     model.add(Dense(200))
-    model.add(Activationi('relu'))
+    model.add(Activation('relu'))
     
     # output
     model.add(Dense(1))
@@ -62,7 +71,7 @@ def network():
     return model
 
 
-def get_sample_list(fname):
+def get_samples_list(fname):
     samples = []
     with open(fname, 'r') as csvfile:
         reader = csv.reader(csvfile)
@@ -81,18 +90,18 @@ def datagen(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                fname = os.path.join('.', 'record', 'IMG', batch_sample[0].split(os.path.sep)[-1])
+                fname = os.path.join('.', 'record', 'IMG', batch_sample[0].split('\\')[-1])
                 center_im = mpim.imread(fname)
                 center_th = float(batch_sample[3])
                 images.append(center_im)
                 angles.append(center_th)
                 
             images = np.array(images)
-            angles = np.array(images)
+            angles = np.array(angles)
             yield shuffle(images, angles)
 
             
-def train(model, batch_size=32):
+def train(model, batch_size=32, epochs=5):
     fname = os.path.join('.', 'record', 'driving_log.csv')
     samples = get_samples_list(fname)
     train_samples, valid_samples = train_test_split(samples, test_size=0.25)
@@ -103,8 +112,12 @@ def train(model, batch_size=32):
     model = network()
     history = model.fit_generator(train_gen, steps_per_epoch=math.ceil(len(train_samples)/batch_size),
                                   validation_data=valid_gen, validation_steps=math.ceil(len(valid_samples)/batch_size),
-                                  epochs=5, vebose=1)
+                                  epochs=epochs, verbose=1)
     model.save('model.h5')
+    return history
 
 if __name__ == "__main__":
     print("Hello")
+    mdl = network()
+    print("Start training!")
+    hst = train(mdl)
